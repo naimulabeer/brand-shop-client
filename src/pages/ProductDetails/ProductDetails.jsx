0dc7f06c02e4465a972a64d881dced15e934aaa4
naimulabeer <naimul.h.abeer@gmail.com>
2023-10-19 03:55:52 +0600

    creating a slider for details page

--- src/pages/ProductDetails/ProductDetails.jsx
+++ src/pages/ProductDetails/ProductDetails.jsx
@@ -1,12 +1,45 @@
+/* eslint-disable no-unused-vars */
 import { useLoaderData } from "react-router-dom";
 import Rating from "../../components/Rating/Rating";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 
 function ProductDetails() {
   const products = useLoaderData();
 
   return (
     <>
-      <div className="grid lg:grid-cols-4 md:grid-cols-2 gap-4 px-4">
+      <div className="hidden md:block m-auto md:w-1/2 md:h-1/2">
+        <Swiper
+          modules={[Navigation, Pagination, Scrollbar, A11y]}
+          spaceBetween={50}
+          slidesPerView={1}
+          navigation
+          pagination={{ clickable: true }}
+          scrollbar={{ draggable: true }}
+          onSlideChange={() => {}}
+          onSwiper={(swiper) => {}}
+        >
+          <SwiperSlide>
+            <img src="/slider1.jpg" alt="slider1" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="/slider2.jpg" alt="slider2" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="/slider3.jpg" alt="slider3" />
+          </SwiperSlide>
+        </Swiper>
+      </div>
+      <h1 className="mt-10 mb-10 text-center text-3xl font-bold border border-b-1 border-b-slate-500 bg-slate-400">
+        All Products Here
+      </h1>
+      <div className="mb-10 grid lg:grid-cols-4 md:grid-cols-2 gap-4 px-4">
         {products.length === 0 ? (
           <span className="col-span-2">No products found for this brand.</span>
         ) : (
@@ -21,7 +54,11 @@ function ProductDetails() {
                 className=" w-48 h-48 mx-auto"
               />
 
-              <p className="text-xl font-bold mt-4 text-center">
+              <p className="text-lg font-bold mt-4 text-center">
+                {product.name}
+              </p>
+
+              <p className="font-bold mt-4 text-center">
                 Price: {product.price} Tk.
               </p>
 
